a701a87be607b2f4adb397fcedfe5e6201572745
rg <1aab@protonmail.com>
2021-04-17 09:07:04 -0300

    update CHANGELOG.txt

--- src/http-client.js
+++ src/http-client.js
@@ -17,15 +17,15 @@ const PromiseRequest = (httpMethod, host, path, options) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     const { headers, timeout, params, body } = options;
-    xhr.httpClient = Object.create(null);
+    xhr.httpclient = Object.create(null);
     xhr.open(httpMethod, `${host}${path}${createQueryString(params)}`, true);
     for (let key in headers) {
       xhr.setRequestHeader(key, headers[key]);
     }
     xhr.timeout = timeout;
-    xhr.addEventListener('abort'  , () => { xhr.httpClient.cause = 'abort';   reject(xhr) });
-    xhr.addEventListener('timeout', () => { xhr.httpClient.cause = 'timeout'; reject(xhr) });
-    xhr.addEventListener('error'  , () => { xhr.httpClient.cause = 'error';   reject(xhr) });
+    xhr.addEventListener('abort'  , () => { xhr.httpclient.cause = 'abort';   reject(xhr) });
+    xhr.addEventListener('timeout', () => { xhr.httpclient.cause = 'timeout'; reject(xhr) });
+    xhr.addEventListener('error'  , () => { xhr.httpclient.cause = 'error';   reject(xhr) });
     xhr.addEventListener('readystatechange', () => {
       if (xhr.readyState !== xhr.DONE || xhr.status === 0) {
         return;
@@ -33,7 +33,7 @@ const PromiseRequest = (httpMethod, host, path, options) => {
       if (/^2\d{2}$/.test(String(xhr.status))) {
         resolve(xhr)
       } else {
-        xhr.httpClient.cause = 'status';
+        xhr.httpclient.cause = 'status';
         reject(xhr);
       };
     });
@@ -41,7 +41,7 @@ const PromiseRequest = (httpMethod, host, path, options) => {
   });
 };
 
-const HttpClient = function(host = '', defaultOptions = {}) {
+const httpclient = function(host = '', defaultOptions = {}) {
   this.head  = (path, options = {}) => this.request('HEAD', path, options);
   this.get   = (path, options = {}) => this.request('GET', path, options);
   this.post  = (path, options = {}) => this.request('POST', path, options);
@@ -56,5 +56,5 @@ const HttpClient = function(host = '', defaultOptions = {}) {
   return this;
 };
 
-module.exports = HttpClient;
-module.exports.default = HttpClient;
+module.exports = httpclient;
+module.exports.default = httpclient;
