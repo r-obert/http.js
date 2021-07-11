937aa83a38be25e21e59d4baa182fa1816485b8a
rg <1aab@protonmail.com>
2021-07-11 10:13:02 -0300

    add "yarn run linter"

--- spec/http-client.spec.js
+++ spec/http-client.spec.js
@@ -1,4 +1,4 @@
-import httpclient from '../src/http-client.js';
+import HttpClient from '../src/http-client.js';
 import mock from 'xhr-mock';
 
 describe('httpclient', () => {
@@ -7,87 +7,85 @@ describe('httpclient', () => {
 
   describe('GET', () => {
     test('a successful GET request to /index.html', (done) => {
-      const client = new httpclient();
+      const client = new HttpClient();
       expect.assertions(1);
 
       mock.get('/index.html', (req, res) => {
-        return res.status(200).body("foobar");
+        return res.status(200).body('foobar');
       });
 
       client
-      .get('/index.html')
-      .then((xhr) => {
-        expect(xhr.responseText).toBe("foobar");
-        done();
-      });
+        .get('/index.html')
+        .then((xhr) => {
+          expect(xhr.responseText).toBe('foobar');
+          done();
+        });
     });
 
     test('a successful GET request to /index.html?q=foo%20bar', (done) => {
-      const client = new httpclient();
+      const client = new HttpClient();
       expect.assertions(1);
 
       mock.get('/index.html?q=foo%20bar', (req, res) => {
-        return res.status(200).body("foobar");
+        return res.status(200).body('foobar');
       });
 
       client
-      .get('/index.html', {params: {q: 'foo bar'}})
-      .then((xhr) => {
-        expect(xhr.responseText).toBe("foobar");
-        done();
-      });
+        .get('/index.html', { params: { q: 'foo bar' } })
+        .then((xhr) => {
+          expect(xhr.responseText).toBe('foobar');
+          done();
+        });
     });
 
     test('a successful GET request to /index.html with the Accept header', (done) => {
-      const client = new httpclient();
+      const client = new HttpClient();
       expect.assertions(1);
 
       mock.get('/index.html', (req, res) => {
         const reqHeaders = req.headers();
-        expect(reqHeaders.accept).toBe("text/html");
-        return res.status(200).body("foobar");
+        expect(reqHeaders.accept).toBe('text/html');
+        return res.status(200).body('foobar');
       });
 
       client
-      .get('/index.html', {headers: {accept: 'text/html'}})
-      .then(() => done());
+        .get('/index.html', { headers: { accept: 'text/html' } })
+        .then(() => done());
     });
 
     test('an unsuccessful GET request to /index.html', (done) => {
-      const client = new httpclient();
+      const client = new HttpClient();
       expect.assertions(1);
 
       mock.get('/index.html', (req, res) => {
-        return res.status(500).body("server error");
+        return res.status(500).body('server error');
       });
 
       client
-      .get('/index.html', {headers: {accept: 'text/html'}})
-      .catch((xhr) => {
-        expect(xhr.httpclient.cause).toBe("status");
-        done();
-      });
-    })
+        .get('/index.html', { headers: { accept: 'text/html' } })
+        .catch((xhr) => {
+          expect(xhr.httpclient.cause).toBe('status');
+          done();
+        });
+    });
   });
 
   describe('POST', () => {
     test('a POST request to /posts', (done) => {
-      const client = new httpclient();
+      const client = new HttpClient();
       expect.assertions(2);
 
       mock.post('/posts', (req, res) => {
-        expect(req.body()).toBe(JSON.stringify({body: "foobar"}))
-        return res.status(200).body(JSON.stringify({status: "ok"}));
+        expect(req.body()).toBe(JSON.stringify({ body: 'foobar' }));
+        return res.status(200).body(JSON.stringify({ status: 'ok' }));
       });
 
       client
-      .post('/posts', {body: JSON.stringify({body: "foobar"})})
-      .then((xhr) => {
-        expect(xhr.responseText).toBe(JSON.stringify({status: "ok"}));
-        done();
-      });
+        .post('/posts', { body: JSON.stringify({ body: 'foobar' }) })
+        .then((xhr) => {
+          expect(xhr.responseText).toBe(JSON.stringify({ status: 'ok' }));
+          done();
+        });
     });
-  })
+  });
 });
-
-
--- src/http-client.js
+++ src/http-client.js
@@ -4,8 +4,8 @@ const encode = (str) => {
 
 const createQueryString = (params) => {
   if (Object.keys(params || {}).length > 0) {
-    let query = [];
-    for (let key in params) {
+    const query = [];
+    for (const key in params) {
       query.push(`${encode(key)}=${encode(params[key])}`);
     }
     return '?' + query.join('&');
@@ -19,41 +19,41 @@ const PromiseRequest = (httpMethod, baseURI, path, options) => {
     const { headers, timeout, params, body } = options;
     xhr.httpclient = Object.create(null);
     xhr.open(httpMethod, `${baseURI}${path}${createQueryString(params)}`, true);
-    for (let key in headers) {
+    for (const key in headers) {
       xhr.setRequestHeader(key, headers[key]);
     }
     xhr.timeout = timeout;
-    xhr.addEventListener('abort'  , () => { xhr.httpclient.cause = 'abort';   reject(xhr) });
-    xhr.addEventListener('timeout', () => { xhr.httpclient.cause = 'timeout'; reject(xhr) });
-    xhr.addEventListener('error'  , () => { xhr.httpclient.cause = 'error';   reject(xhr) });
+    xhr.addEventListener('abort', () => { xhr.httpclient.cause = 'abort'; reject(xhr); });
+    xhr.addEventListener('timeout', () => { xhr.httpclient.cause = 'timeout'; reject(xhr); });
+    xhr.addEventListener('error', () => { xhr.httpclient.cause = 'error'; reject(xhr); });
     xhr.addEventListener('readystatechange', () => {
       if (xhr.readyState !== xhr.DONE || xhr.status === 0) {
         return;
       }
       if (/^2\d{2}$/.test(String(xhr.status))) {
-        resolve(xhr)
+        resolve(xhr);
       } else {
         xhr.httpclient.cause = 'status';
         reject(xhr);
-      };
+      }
     });
     xhr.send(body);
   });
 };
 
-export default function(defaultOptions = {}) {
+export default function (defaultOptions = {}) {
   const baseURI = defaultOptions.baseURI || '';
 
-  this.head  = (path, options = {}) => this.request('HEAD', path, options);
-  this.get   = (path, options = {}) => this.request('GET', path, options);
-  this.post  = (path, options = {}) => this.request('POST', path, options);
-  this.put   = (path, options = {}) => this.request('PUT', path, options);
+  this.head = (path, options = {}) => this.request('HEAD', path, options);
+  this.get = (path, options = {}) => this.request('GET', path, options);
+  this.post = (path, options = {}) => this.request('POST', path, options);
+  this.put = (path, options = {}) => this.request('PUT', path, options);
   this.patch = (path, options = {}) => this.request('PATCH', path, options);
 
   this.request = (httpMethod, path, options = {}) => {
-    options = Object.assign({}, defaultOptions, options)
-    return PromiseRequest(httpMethod, baseURI, path, options)
+    options = Object.assign({}, defaultOptions, options);
+    return PromiseRequest(httpMethod, baseURI, path, options);
   };
 
   return this;
-};
+}
